a8a946545c65cd92e7099a98189959cbe9048d50
Charles Arboleda <charlesarboleda_@hotmail.com>
2022-11-01 00:15:33 +0000

    Added generateRandomString functionality

--- express_server.js
+++ express_server.js
@@ -1,7 +1,19 @@
+const generateRandomString = () => {
+  let randomString = "";
+  let alphaNumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  let alphaNumericLength = alphaNumeric.length;
+
+  for (let i = 0; i < 6; i++) {
+    randomString += alphaNumeric.charAt(Math.floor(Math.random() * alphaNumericLength)); 
+  }
+  return randomString;
+};
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -23,4 +35,23 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
