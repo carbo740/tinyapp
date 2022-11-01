5f05d93d6d16d534e6aa35cea52c2bf0591bd5bb
Charles Arboleda <charlesarboleda_@hotmail.com>
2022-11-01 00:45:37 +0000

    Added redirect after form submission functionality

--- express_server.js
+++ express_server.js
@@ -46,12 +46,13 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:id", (req, res) => {
+app.get(`/urls/:id`, (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  let id = generateRandomString();
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls/${id}`); // Redirects to /urls/:(shortened ID)
 });
